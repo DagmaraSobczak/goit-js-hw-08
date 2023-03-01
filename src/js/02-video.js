ebca6a57826a0ddf3553c574fb1504f63b7c788e
DagmaraSobczak <daga.sobczak@gmail.com>
2023-03-01 22:11:53 +0100

    task 2 vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Vimeo from '@vimeo/player';
 
+const player = new Vimeo('vimeo-player');
+
+player.on('timeupdate', function (data) {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+});
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
