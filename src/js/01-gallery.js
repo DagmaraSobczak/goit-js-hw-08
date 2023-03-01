e43ba18deabd53057041994863077d98a0b3e83a
DagmaraSobczak <daga.sobczak@gmail.com>
2023-03-01 20:18:43 +0100

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createGalleryItem(item) {
+  const galleryItem = document.createElement('div');
+  galleryItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.href = item.original;
+
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.src = item.preview;
+  galleryImage.dataset.source = item.original;
+  galleryImage.alt = item.description;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+}
+
+for (let i = 0; i < galleryItems.length; i++) {
+  const galleryItem = createGalleryItem(galleryItems[i]);
+  gallery.appendChild(galleryItem);
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionSrc: 'alt',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
