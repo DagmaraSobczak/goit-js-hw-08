437eebc184eb64c97ae87f757ba092f23c962df6
DagmaraSobczak <daga.sobczak@gmail.com>
2023-03-03 14:09:32 +0100

    lodash.throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,18 @@
 import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const player = new Vimeo('vimeo-player');
 
+function saveCurrentTimeToLocalStorage(currentTime) {
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+const throttledSaveCurrentTimeToLocalStorage = throttle(
+  saveCurrentTimeToLocalStorage,
+  1000
+);
 player.on('timeupdate', function (data) {
   const currentTime = data.seconds;
-  localStorage.setItem('videoplayer-current-time', currentTime);
+  throttledSaveCurrentTimeToLocalStorage(currentTime);
 });
 const savedTime = localStorage.getItem('videoplayer-current-time');
 if (savedTime) {
